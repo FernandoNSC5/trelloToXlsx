7b8b9971accba6fae0a85f6bf37d80da030cd474
Fernando <fernandonsc5@gmail.com>
2020-02-23 23:53:24 -0300

    Dados class alias

---
+++ data.py
@@ -0,0 +1,36 @@
+class Data:
+
+	def __init__(self):
+		self.buffer = list()
+		self.load_data()
+		self.clean_buffer()
+
+	######################################################################
+	##	METHODS
+	def load_data(self):
+		f = open('dados.txt', 'r')
+		for i in range(5):
+			self.buffer.append(f.readline())
+
+	def clean_buffer(self):
+		for i in range(len(self.buffer)-1):
+			self.buffer[i] = self.buffer[i].replace(self.buffer[i][-1], "")
+			
+	#######################################################################
+	##	GETTERS AND SETTERS
+	def get_api_key(self):
+		return self.buffer[0]
+
+	def get_api_secret(self):
+		return self.buffer[1]
+
+	def get_token(self):
+		return self.buffer[2]
+
+	def get_board_name(self):
+		return self.buffer[3]
+
+	def get_list_name(self):
+		return self.buffer[4]
+
+d = Data()
