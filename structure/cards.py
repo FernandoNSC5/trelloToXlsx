76a261603220ba0b6d401fd3c297ed6dc956998b
znogfer <fncosta@fitec.org.br>
2020-02-28 15:00:25 -0300

    Sort by day method, handle for all data-structures

--- structure/cards.py
+++ structure/cards.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Card:
 
 	def __init__(self, users):
@@ -5,8 +7,16 @@ class Card:
 		for i in users:
 			self.usersDict[i] = list()
 
+	######################################
+	# Methods
+	def sort_dict_lists(self):
+		self.UserDict.sort(key=lambda x: datetime.datetime.strptime(x['Prazo'], '%d/%m/%Y'))
+
 	def addCard(self, user, card):
 		self.usersDict[user].append(card)
+
+	######################################
+	# Getts and Setters
 
 	def getCardList(self, user):
 		return self.usersDict[user]
